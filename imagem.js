63817b4a7d762c3482cd4c9c821aa1514e22db67
onepaceptbr <contaps3.250897@hotmail.com>
2024-10-14 12:16:20 -0300

    Update imagem.js

--- imagem.js
+++ imagem.js
@@ -1,4 +1,4 @@
-const imgSrc = "https://ugc.production.linktr.ee/c951f80b-6809-4a1b-8001-dfc1819a7247_2024-10-14-09-42-50-Water-Seven-06.mkv---Reprodutor-de-M-dias-VLC.png";
+const imgSrc = "https://static.wikia.nocookie.net/onepiece/images/2/21/Diesel_adquire_farinha.png/revision/latest?cb=20220421162704&path-prefix=pt";
 
 document.addEventListener("DOMContentLoaded", () => {
     const imageElements = document.querySelectorAll(".foxy-img");
