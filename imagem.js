764686f8f5f7adca01db5936a169d47cde753c44
onepaceptbr <contaps3.250897@hotmail.com>
2024-10-14 12:18:58 -0300

    Update imagem.js

--- imagem.js
+++ imagem.js
@@ -1,4 +1,4 @@
-const imgSrc = "https://static.wikia.nocookie.net/onepiece/images/2/21/Diesel_adquire_farinha.png/revision/latest?cb=20220421162704&path-prefix=pt";
+const imgSrc = "https://static.wikia.nocookie.net/onepiece/images/a/ad/Diesel_Anime_Infobox.png/revision/latest?cb=20240725230822&path-prefix=pt";
 
 document.addEventListener("DOMContentLoaded", () => {
     const imageElements = document.querySelectorAll(".foxy-img");
