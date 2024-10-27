633eea34ea0aa4cbc7983302952ec8cc4b7654d6
legendasptbronepace <legendasbronepace@gmail.com>
2024-10-26 22:25:40 -0300

    imagem.js

--- imagem.js
+++ imagem.js
@@ -1,4 +1,4 @@
-const imgSrc = "https://ugc.production.linktr.ee/14c48458-874d-4957-a0c7-ec8d893a6681_2024-10-18-10-55-48-25b24c4febab1fc9ddb1b06405f63c08.jpg--1200-675-.png?io=true&size=avatar-v3_0";
+const imgSrc = "https://ugc.production.linktr.ee/32df9299-f26f-4813-b43d-752d7c3c353a_brook-one-piece-378.jpeg";
 
 document.addEventListener("DOMContentLoaded", () => {
     const imageElements = document.querySelectorAll(".foxy-img");
