6bbee58a5d2876bfa909b9de8decd470b53d9a38
legendasptbronepace <legendasbronepace@gmail.com>
2024-11-05 14:34:45 -0300

    imagem.js

--- imagem.js
+++ imagem.js
@@ -1,4 +1,4 @@
-const imgSrc = "https://ugc.production.linktr.ee/7a9d5976-d64a-42d3-b8df-f7e42c53d68a_508688cb93baf902215c7ef01d4102ec.jpeg?io=true&size=avatar-v3_0";
+const imgSrc = "https://ugc.production.linktr.ee/d44d1895-9c37-47a7-a578-c8ffe416ff5c_2024-11-05-14-33-15-Sabaody-Archipelago-01.mkv---Reprodutor-de-M-dias-VLC.png?io=true&size=avatar-v3_0";
 
 document.addEventListener("DOMContentLoaded", () => {
     const imageElements = document.querySelectorAll(".foxy-img");
