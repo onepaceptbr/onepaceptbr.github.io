52a1f914e9ecf360edba8cd19c5dcf36f31338d4
legendasptbronepace <legendasbronepace@gmail.com>
2024-11-06 19:26:23 -0300

    imagem.js

--- imagem.js
+++ imagem.js
@@ -1,4 +1,4 @@
-const imgSrc = "https://ugc.production.linktr.ee/d44d1895-9c37-47a7-a578-c8ffe416ff5c_2024-11-05-14-33-15-Sabaody-Archipelago-01.mkv---Reprodutor-de-M-dias-VLC.png?io=true&size=avatar-v3_0";
+const imgSrc = "https://geekdama.com.br/wp-content/uploads/2023/11/one-piece-episodio-405-arquipelago-sabaody-luffy-choro-desespero-postcover.jpg";
 
 document.addEventListener("DOMContentLoaded", () => {
     const imageElements = document.querySelectorAll(".foxy-img");
