084512cdee7b851b324f0c6ac83e7522414db7af
legendasptbronepace <legendasbronepace@gmail.com>
2024-11-09 17:38:43 -0300

    imagem.js

--- imagem.js
+++ imagem.js
@@ -1,4 +1,4 @@
-const imgSrc = "https://geekdama.com.br/wp-content/uploads/2023/11/one-piece-episodio-405-arquipelago-sabaody-luffy-choro-desespero-postcover.jpg";
+const imgSrc = "https://ugc.production.linktr.ee/51964943-7425-4d76-8b9a-f6b098ca0d17_2024-11-09-17-35-37-Impel-Down-01.mkv---Reprodutor-de-M-dias-VLC.png?io=true&size=avatar-v3_0";
 
 document.addEventListener("DOMContentLoaded", () => {
     const imageElements = document.querySelectorAll(".foxy-img");
